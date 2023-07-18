5956e96149fd40e41283156b082593c6b4fe0286
Aruna k s <36861106+aruna-k-s@users.noreply.github.com>
2023-07-18 07:37:24 +0000

    commit changes on dropdown changes

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -3,12 +3,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.css']
+  styleUrls: ['./dropdown.component.css'],
 })
 export class DropdownComponent {
-
-  @Input() options: string[] = [];
-  @Input() placeholder: string = '';
+  @Input() options: string[] = []; // list of options recieved from the parent
+  @Input() placeholder: string = ''; // placeholder recived from the parent
   @Input() value: string = ''; // Input binding for the selected value
 
   @Output() valueChange: EventEmitter<string> = new EventEmitter<string>(); // Output binding for value changes
@@ -17,11 +16,11 @@ export class DropdownComponent {
   // Method triggered when a new option is selected
   onOptionSelected(option: string) {
     this.value = option;
+    this.toggleDropdown();
     this.valueChange.emit(option);
   }
-
-  toggleDropdown(){
+// function toggeling the dropdown when user click on the options
+  toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
-
 }
